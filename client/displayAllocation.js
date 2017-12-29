dbdb26543ee6c288c531620948df16804de81991
Mark Solters <msolters@gmail.com>
2017-12-28 21:14:06 -0500

    Improve Allocation Chart
    Sort series by name to prevent "jumping" when new data is synced.  Don't
    render graph until CSS grid is drawn

--- client/displayAllocation.js
+++ client/displayAllocation.js
@@ -1,55 +1,60 @@
 Template.displayAllocation.onRendered( function() {
-  $('#coin-allocation-chart').highcharts({
-    chart: {
-        type: 'pie',
-        backgroundColor: null,
-        style: {
-          color: "#ffffff"
-        }
-    },
-    legend: {
-      itemStyle: {
-        color: '#fff'
-      }
-    },
-    title: {
-        text: 'Portfolio Allocation',
-        style: {
+  const self = this
+    Meteor.setTimeout( function() {
+    $('#coin-allocation-chart').highcharts({
+      chart: {
+          type: 'pie',
+          backgroundColor: null,
+          style: {
+            color: "#ffffff"
+          }
+      },
+      legend: {
+        itemStyle: {
           color: '#fff'
         }
-    },
-    plotOptions: {
-      pie: {
-        allowPointSelect: true,
-        cursor: 'pointer',
-        dataLabels: {
-            enabled: true,
-            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-            style: {
-                color: "#fff"
-            }
+      },
+      title: {
+          text: 'Portfolio Allocation',
+          style: {
+            color: '#fff'
+          }
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+              style: {
+                  color: "#fff"
+              }
+          }
         }
+      },
+      series: [{
+        name: 'USD'
+      }]
+    });
+
+    self.autorun( () => {
+      let portfolio = Portfolio.findOne()
+      if( !portfolio ) return
+
+      let chart = $("#coin-allocation-chart").highcharts()
+      let serie_data = []
+      for( let [coin, coin_data] of Object.entries(portfolio.coins) ) {
+        serie_data.push({
+          name: coin_data.symbol,
+          y: coin_data.coin_value
+        })
       }
-    },
-    series: [{
-      name: 'USD'
-    }]
-  });
 
-  this.autorun( () => {
-    let portfolio = Portfolio.findOne()
-    if( !portfolio ) return
+      serie_data = _.sortBy(serie_data, s => s.name)
 
-    let chart = $("#coin-allocation-chart").highcharts()
-    let serie_data = []
-    for( let [coin, coin_data] of Object.entries(portfolio.coins) ) {
-      serie_data.push({
-        name: coin_data.symbol,
-        y: coin_data.coin_value
-      })
-    }
-
-    chart.series[0].setData(serie_data)
-    chart.redraw()
-  })
+      chart.series[0].setData(serie_data)
+      chart.redraw()
+    })
+  }, 100)
 })
