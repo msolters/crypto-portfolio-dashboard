016d7b7e7d042b98ccfd9b11b7c72e8878574c8d
Mark Solters <msolters@gmail.com>
2017-12-29 19:23:13 -0500

    Add return performance line chart

---
+++ client/performanceChart.js
@@ -0,0 +1,120 @@
+Template.performanceChart.onCreated( function() {
+  this.subscribe('LastDay')
+})
+
+Template.performanceChart.onRendered( function() {
+  const self = this;
+  Meteor.setTimeout( function() {
+    $('#performance-chart').highcharts({
+      chart: {
+          type: 'line',
+          backgroundColor: null,
+          style: {
+            color: "#ffffff"
+          }
+      },
+      legend: {
+        itemStyle: {
+          color: '#fff'
+        },
+        layout: "vertical",
+        align: "center",
+        verticalAlign: "bottom"
+      },
+      title: {
+          text: 'Return Performance',
+          style: {
+            color: '#fff'
+          }
+      },
+      subtitle: {
+          text: 'Last 24 Hours',
+          style: {
+            color: '#fff'
+          }
+      },
+      xAxis: {
+        type: 'datetime',
+        crosshair: true,
+        title: {
+          text: 'Time',
+          style: {
+            color: '#fff'
+          }
+        },
+        labels: {
+          style: {
+            color: "#fff"
+          }
+        }
+      },
+      yAxis: {
+        title: {
+          text: '%',
+          style: {
+            color: '#fff'
+          }
+        },
+        labels: {
+          formatter: function () {
+            return this.value.toFixed(0) + '%';
+          },
+          style: {
+            color: '#fff'
+          }
+        }
+      },
+      tooltip: {
+        pointFormat: '{point.y:.2f}%<br>'
+      },
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          marker: {
+            enabled: false
+          }
+        }
+      },
+      series: []
+    });
+
+    self.autorun( () => {
+      let chart = $("#performance-chart").highcharts()
+
+      // Get snapshots
+      let snapshots_q = {}
+      let snapshots = PortfolioSnapshots.find(snapshots_q, {sort: {
+        ts: -1
+      }, limit: 96}).fetch()
+
+      // Generate new series data
+      let now = new Date()
+      let tzOffset = now.getTimezoneOffset() * 60000
+      let series_data = {}
+      for( let s of snapshots.reverse() ) {
+        let new_point = [s.ts.valueOf()-tzOffset, s.performance]
+        if( series_data['performance'] ) {
+          series_data['performance'].push( new_point )
+        } else {
+          series_data['performance'] = [ new_point ]
+        }
+      }
+
+      for( let [serie, data] of Object.entries(series_data) ) {
+        let series_exist = _.find(chart.series, (s) => {
+          return (s.name === serie)
+        })
+        if( series_exist ) {
+          series_exist.setData(data)
+        } else {
+          chart.addSeries({
+            name: serie,
+            data: data
+          })
+        }
+      }
+
+      chart.redraw()
+    })
+  }, 100)
+})
